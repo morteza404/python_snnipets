4e1c94c305581d128f6c0166b7866ba3c6596480
shahbazi <mortezash404@gmail.com>
2024-07-04 08:13:33 +0330

    Ruff format

--- python_features/batched_feaure.py
+++ python_features/batched_feaure.py
@@ -1,12 +1,14 @@
 from itertools import islice
+
 """
 python 3.12:
             from itertools import batched
 """
 
+
 def batched(iterable, n):
     "Batch data into tuples of length n. The last batch may be shorter."
-    # batched('ABCDEFG', 3) --> ABC DEF G
+    # batched("ABCDEFG", 3) --> ABC DEF G
     if n < 1:
         raise ValueError("n must be at least one")
     it = iter(iterable)
--- python_features/pydantic_powerful.py
+++ python_features/pydantic_powerful.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel, field_validator
 import datetime as dt
 
+
 class Order(BaseModel):
     id: int
     created_at: dt.datetime
@@ -10,8 +11,8 @@ class Order(BaseModel):
     symbol: str
     side: str
 
-    # class Config:
-    #     validate_assignment = True
+    class Config:
+        validate_assignment = True
 
     @field_validator("quantity")
     def validate_quantity(cls, value):
@@ -19,14 +20,23 @@ class Order(BaseModel):
             raise ValueError("Quantity must be an integer")
         if value < 0:
             raise ValueError("Quantity cannot be negative")
-        return value 
-    
+        return value
+
     @field_validator("created_at")
     def validate_created_at(cls, value):
         if value > dt.datetime.now():
             raise ValueError("Created_at cannot be in the future")
         return value
-    
+
+
 if __name__ == "__main__":
-    order = Order(id=1, created_at=dt.datetime.now(), status="open", price=100.0, quantity=10, symbol="AAPL", side="buy")
+    order = Order(
+        id=1,
+        created_at=dt.datetime.now(),
+        status="open",
+        price=100.0,
+        quantity=10,
+        symbol="AAPL",
+        side="buy",
+    )
     print(order)
