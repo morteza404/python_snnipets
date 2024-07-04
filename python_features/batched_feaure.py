df64d9c47a975f41c513ac693d0c3fb274ad68d0
shahbazi <mortezash404@gmail.com>
2024-07-04 08:02:44 +0330

    Add batched_feaure.py

---
+++ python_features/batched_feaure.py
@@ -0,0 +1,18 @@
+from itertools import islice
+"""
+python 3.12:
+            from itertools import batched
+"""
+
+def batched(iterable, n):
+    "Batch data into tuples of length n. The last batch may be shorter."
+    # batched('ABCDEFG', 3) --> ABC DEF G
+    if n < 1:
+        raise ValueError("n must be at least one")
+    it = iter(iterable)
+    while batch := tuple(islice(it, n)):
+        yield batch
+
+
+for i in batched("ABCDEFG", 3):
+    print(i)
